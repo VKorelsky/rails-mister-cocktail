98a9db66fc8fa1978c1755896b7dd4ba373b3964
Victor Korelsky <victor.korelsky@mail.mcgill.ca>
2016-08-11 14:30:22 +0200

    Cocktail controllers update

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,14 +1,19 @@
 class CocktailsController < ApplicationController
   def index
+    @cocktails = Cocktail.all
   end
 
   def show
+    @cocktail = Cocktail.find(params[:id])
   end
 
   def new
+    @cocktail = Cocktail.new
   end
 
   def create
+    raise
+    Cocktail.create(cocktail_params)
   end
 
   def edit
@@ -18,5 +23,11 @@ class CocktailsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
   end
 end
