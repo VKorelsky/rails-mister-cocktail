79ba349c37a6213dfa4689fef9f09fc49127f237
Victor Korelsky <victor.korelsky@mail.mcgill.ca>
2016-08-11 12:55:12 +0200

    generated new updated controllers

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,8 +1,20 @@
 class CocktailsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
   def new
   end
 
   def create
+  end
+
+  def edit
+  end
+
+  def update
   end
 
   def destroy
