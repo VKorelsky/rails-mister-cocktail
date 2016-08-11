ada5240a1d8e36e7d4158a5ec4c05b9772cfc2e3
Victor Korelsky <victor.korelsky@mail.mcgill.ca>
2016-08-11 15:40:24 +0200

    Progress on dose logic

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -5,8 +5,21 @@ class DosesController < ApplicationController
   end
 
   def create
+    if Dose.create(dose_params)
+      redirect_to cocktail_path(params[:cocktail_id].to_i)
+    else
+      render :new
+    end
   end
 
   def destroy
+    raise
+  end
+
+  private
+
+  def dose_params
+    params[:dose].store("cocktail_id", params[:cocktail_id].to_i)
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
   end
 end
