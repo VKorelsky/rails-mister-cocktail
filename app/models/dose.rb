d5373aa4cc7dab629075c79706135f0f67d5fd84
Victor Korelsky <victor.korelsky@mail.mcgill.ca>
2016-08-11 12:25:26 +0200

    Database models done

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -3,8 +3,10 @@ class Dose < ApplicationRecord
   belongs_to :ingredient
 
   # validations
-  validates :description, presence: true
+  validates :description, presence: true, allow_blank: false
   validates :cocktail, presence: true
   validates :ingredient, presence: true
+
+  validates_uniqueness_of :cocktail, :scope => :ingredient
 
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,8 +1,10 @@
 class Ingredient < ApplicationRecord
-  has_many :doses
+  has_many :doses, :dependent => :restrict_with_exception
   has_many :cocktails, through: :dose
 
   # validations
   validates :name, presence: true
   validates :name, uniqueness: true
+
 end
+
